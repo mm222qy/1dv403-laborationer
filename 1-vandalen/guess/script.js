969c39cd8ca09d330c19ec0f9d3d592fc23b4b45
mm222qy <mm222qy@student.lnu.se>
2014-11-10 15:09:37 +0000

    finished Guess secret number

--- 1-vandalen/guess/script.js
+++ 1-vandalen/guess/script.js
@@ -1,16 +1,31 @@
 "use strict";
 
 window.onload = function(){
-	
-	var secret = 50; // Detta tal behöver bytas ut mot ett slumpat tal.
-	
+	var max = 100;
+	var min = 0;
+	var secret = Math.floor( Math.random() * (max-min)+1 )+min; Math.floor( Math.random() * (100-1)+1) + 1; Math.floor( Math.random() * 100)+1; // Detta tal behöver bytas ut mot ett slumpat tal.
+	var counter;
 	// I denna funktion ska du skriva koden för att hantera "spelet"
 	var guess = function(number){
 		console.log("Det hemliga talet: " + secret); // Du når den yttre variabeln secret innifrån funktionen.
 		console.log("Du gissade: " + number); // Detta nummer är det som användaren gissade på.
 			
 		// Plats för förändring.
-
+		if (number < min || number > max){
+			return[false, "Skriv in ett tal mellan 0-100"]
+		}
+         else if (number == secret){
+         return[true,"Grattis du vann! Det hemliga talet var  och du behövde  gissningar för att hitta det."]
+         }
+         else if (number > secret){
+         	counter+=1;
+         	 return[false, "Det hemliga talet är mindre"]
+         }
+         else if (number < secret){
+         	counter+=1;
+         	 return[false, "Det hemliga talet är större"]
+         }
+         
 
 		// Returnera exempelvis: 
 		// [true, "Grattis du vann! Det hemliga talet var X och du behövde Y gissningar för att hitta det."]
