dd1881c947714a40bf624be397b0f9f5692544c7
mm222qy <mm222qy@student.lnu.se>
2014-11-11 12:25:54 +0000

    klar tror jag

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -3,7 +3,30 @@
 var makePerson = function(persArr){
 
 
-	// Din kod här...
+	var names = [];
+    var ages = [];
+    var agesSum = 0;
+    for (var i=0; i<persArr.length; i+=1){
+        names.push(persArr[i].name);
+        ages.push(persArr[i].age);
+        agesSum+=ages[i];
+    }
+    names.sort(function(a,b)
+    {
+        return a.localeCompare(b);
+    });
+    ages.sort();
+    
+    var averageAge = agesSum/ages.length;
+    averageAge = Math.round(averageAge);
+    
+    var result = {
+        names:  names.join(", "),
+        minAge: ages[0],
+        maxAge: Math.max.apply(Math, ages),
+        averageAge: averageAge
+    };
 
+    return result;
 }
 
